9a0b348746761a863914976f5bc7f7aed45b75ea
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-05-02 15:17:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function newItem() {
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    $('#list').append(li);
+  }
+
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+
+  li.on('dblclick', crossOut);
+
+  let crossOutButton = $('<button>X</button>');
+  crossOutButton.css('float', 'right');
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', function() {
+    li.addClass('delete');
+  });
+
+  $('#list').sortable();
+}
+
+$(document).ready(function() {
+  $('#button').on('click', newItem);
+  $('#input').on('keypress', function(event) {
+    if (event.which === 13) {
+      event.preventDefault();
+      newItem();
+    }
+  });
+});
